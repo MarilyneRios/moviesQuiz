a147611084b3627b840736c66426d9f3fa8ba914
MarilyneRios <rios.marilyne@gmail.com>
2024-05-10 23:25:07 +0200

    text footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Container } from 'react-bootstrap';
 const Footer = () => {
   return (
     <div className='bg-dark bg-gradient text-white w-100  py-3 position-fixed bottom-0'>
-      <Container >
-        <p className='text-center'>
+      <Container className='text-center'>
+        <p >
           © {new Date().getFullYear()} Movie Quiz App | Exercice d&apos;entrainement frontend avec Vite et React
         </p>
         <p>https://github.com/MarilyneRios/moviesQuiz</p>
